8e194b43de9ef0d328f9e572f081420f50d26d48
Bảo Thắng <148514506+baothanngg@users.noreply.github.com>
2026-09-14 23:06:49 +0700

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    // Time complex: O(n*klogk)
+    // Space complex: O(n*k)
+    groupAnagrams(strs: string[]): string[][] {
+        const group = new Map<string, string[]>();
+
+        for (const str of strs) {
+            const count = new Map<string, number>();
+
+            for (const char of str) {
+                count.set(char, (count.get(char) ?? 0) + 1);
+            }
+
+            const key = [...count.entries()]
+                .sort((a, b) => a[0].localeCompare(b[0])) // [['a', 0], ['b', 0]]
+                .map(([char, freq]) => `${char}:${freq}`) // ['a:0', 'b:0']
+                .join("|");
+
+            if (!group.has(key)) {
+                group.set(key, []);
+            }
+
+            group.get(key).push(str);
+        }
+
+        return [...group.values()];
+    }
+}
