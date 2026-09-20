bfc8c75d4c9c0d1db800e7f6b27d68410d6daa25
Bảo Thắng <148514506+baothanngg@users.noreply.github.com>
2026-09-20 10:34:44 +0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const group = new Map<string, string[]>();
+
+        for (const str of strs) {
+            const key = [...str].sort().join("");
+
+            if (!group.get(key)) group.set(key, []);
+
+            group.get(key).push(str);
+        }
+
+        return [...group.values()];
+    }
+}
