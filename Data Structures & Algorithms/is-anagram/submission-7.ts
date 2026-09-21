a948b10761965b0d7a72e7807302de105946d4de
Bảo Thắng <148514506+baothanngg@users.noreply.github.com>
2026-09-21 22:02:36 +0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+        const map = new Map();
+
+        for (const c of s) {
+            map.set(c, (map.get(c) ?? 0) + 1);
+        }
+
+        for (const c of t) {
+            if (map.get(c) === undefined || map.get(c) === 0) return false;
+
+            map.set(c, map.get(c) - 1);
+        }
+
+        return true;
+    }
+}
