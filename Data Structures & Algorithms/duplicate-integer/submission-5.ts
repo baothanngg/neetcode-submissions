107c37a26e3f92457cdf5349abab71a9cd1a581e
Bảo Thắng <148514506+baothanngg@users.noreply.github.com>
2026-09-21 21:39:04 +0700

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const result = new Set(nums)
+
+        if(result.size !== nums.length)
+            return true
+
+        return false
+    }
+}
