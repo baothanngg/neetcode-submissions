291b542b96452fc35314c5b5a70dd6cbe508ea73
Bảo Thắng <148514506+baothanngg@users.noreply.github.com>
2026-09-22 22:13:36 +0700

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    // Time: On
+    // Space: O1
+    maxArea(heights: number[]): number {
+        let l = 0;
+        let r = heights.length - 1;
+        let max = 0;
+
+        while (l < r) {
+            const height = Math.min(heights[l], heights[r]);
+            const width = r - l;
+            const amount = height * width;
+
+            max = Math.max(max, amount);
+            if (heights[l] < heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return max;
+    }
+}
